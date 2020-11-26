e4b2e4153766dc5880edd0800ab318f1153602cd
LintaoLu <llu25@hawk.iit.edu>
2020-11-25 18:41:32 -0600

    add documentdb

---
+++ documentdb/src/main/java/site/ycsb/db/DocumentDBClient.java
@@ -0,0 +1,44 @@
+package site.ycsb.db;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class DocumentDBClient {
+
+    public DocumentDBClient() {
+        String template = "mongodb://%s:%s@%s/sample-database?replicaSet=rs0&readpreference=%s";
+        String username = "<sample-user>";
+        String password = "<password>";
+        String clusterEndpoint = "sample-cluster.node.us-east-1.docdb.amazonaws.com:27017";
+        String readPreference = "secondaryPreferred";
+        String connectionString = String.format(template, username, password, clusterEndpoint, readPreference);
+
+        MongoClientURI clientURI = new MongoClientURI(connectionString);
+        MongoClient mongoClient = new MongoClient(clientURI);
+
+        MongoDatabase testDB = mongoClient.getDatabase("sample-database");
+        MongoCollection<Document> numbersCollection = testDB.getCollection("sample-collection");
+
+        Document doc = new Document("name", "pi").append("value", 3.14159);
+        numbersCollection.insertOne(doc);
+
+        MongoCursor<Document> cursor = numbersCollection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        DocumentDBClient documentDBClient = new DocumentDBClient();
+    }
+
+}
