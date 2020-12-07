447aeef959a5c7c3a71247796894a3ab1a6df173
LintaoLu <llu25@hawk.iit.edu>
2020-12-07 12:37:31 -0600

    test neptune

--- dynamodb/src/main/java/site/ycsb/db/DynamoDBClient.java
+++ dynamodb/src/main/java/site/ycsb/db/DynamoDBClient.java
@@ -16,22 +16,18 @@
 
 package site.ycsb.db;
 
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.result.DeleteResult;
-import com.mongodb.client.result.UpdateResult;
-import org.bson.Document;
-import org.bson.types.Binary;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.
+        remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.
+        traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.
+        traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.
+        util.empty.EmptyGraph;
 import site.ycsb.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * DynamoDB v1.10.48 client for YCSB.
@@ -39,167 +35,88 @@ import java.util.Vector;
 
 public class DynamoDBClient extends DB {
 
-  private MongoClientURI clientURI;
-  private MongoClient mongoClient;
-  private MongoDatabase testDB;
-  private static final String PK = "_id";
-  private static final Integer INCLUDE = Integer.valueOf(1);
+  private GraphTraversalSource g;
+  private Cluster cluster;
 
-  public void init() {
-    String template = "mongodb://%s:%s@%s/sample-database?replicaSet=rs0&readpreference=%s";
-    String username = "llu25";
-    String password = "huhu87588315";
-    String clusterEndpoint = "llu25.cluster-cbo8ghduq2g8.us-east-1.docdb.amazonaws.com:27017";
-    String readPreference = "secondaryPreferred";
-    String connectionString = String.format(template, username, password, clusterEndpoint, readPreference);
+  @Override
+  public void init() throws DBException {
+    Cluster.Builder builder = Cluster.build();
+    builder.addContactPoint("llu25.cluster-cbo8ghduq2g8.us-east-1.neptune.amazonaws.com");
+    builder.port(8182);
+    builder.enableSsl(true);
+    builder.keyCertChainFile("SFSRootCAG2.pem");
 
+    cluster = builder.create();
 
-    clientURI = new MongoClientURI(connectionString);
-    mongoClient = new MongoClient(clientURI);
-
-    testDB = mongoClient.getDatabase("userdb");
+    g = EmptyGraph.instance().traversal()
+            .withRemote(DriverRemoteConnection.using(cluster));
   }
 
   @Override
-  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
-    try {
-      MongoCollection<Document> collection = testDB.getCollection(table);
-      Document query = new Document(PK, key);
+  public void cleanup() throws DBException {
+    g.V().drop().iterate();
+    cluster.close();
+  }
 
-      FindIterable<Document> findIterable = collection.find(query);
-
-      if (fields != null) {
-        Document projection = new Document();
-        for (String field : fields) {
-          projection.put(field, INCLUDE);
-        }
-        findIterable.projection(projection);
+  @Override
+  public Status read(String table, String key,
+                     Set<String> fields, Map<String, ByteIterator> result) {
+    GraphTraversal<Vertex, Map<Object, Object>> curr = g.V(key).valueMap();
+    while (curr.hasNext()) {
+      Map<Object, Object> e = curr.next();
+      for (Map.Entry<Object, Object> entry : e.entrySet()) {
+        result.put((String) entry.getKey(), new StringByteIterator((String) entry.getValue()));
       }
-
-      Document queryResult = findIterable.first();
-
-      if (queryResult != null) {
-        fillMap(result, queryResult);
-      }
-      return queryResult != null ? Status.OK : Status.NOT_FOUND;
-    } catch (Exception e) {
-      System.err.println(e.toString());
-      return Status.ERROR;
     }
+    return Status.OK;
   }
 
   @Override
   public Status scan(String table, String startkey, int recordcount,
-                     Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-    MongoCursor<Document> cursor = null;
-    try {
-      MongoCollection<Document> collection = testDB.getCollection(table);
-
-      Document scanRange = new Document("$gte", startkey);
-      Document query = new Document(PK, scanRange);
-      Document sort = new Document(PK, INCLUDE);
-
-      FindIterable<Document> findIterable =
-              collection.find(query).sort(sort).limit(recordcount);
-
-      if (fields != null) {
-        Document projection = new Document();
-        for (String fieldName : fields) {
-          projection.put(fieldName, INCLUDE);
-        }
-        findIterable.projection(projection);
+                     Set<String> fields,
+                     Vector<HashMap<String, ByteIterator>> result) {
+    // This gets the vertices, only.
+    GraphTraversal<Vertex, Map<Object, Object>> t =
+            g.V().limit(recordcount).valueMap();
+    while (t.hasNext()) {
+      Map<Object, Object> e = t.next();
+      HashMap<String, ByteIterator> map = new HashMap<>();
+      for (String str : fields) {
+        map.put(str, (ByteIterator) e.get(str));
       }
-
-      cursor = findIterable.iterator();
-
-      if (!cursor.hasNext()) {
-        System.err.println("Nothing found in scan for key " + startkey);
-        return Status.ERROR;
-      }
-
-      result.ensureCapacity(recordcount);
-
-      while (cursor.hasNext()) {
-        HashMap<String, ByteIterator> resultMap =
-                new HashMap<String, ByteIterator>();
-
-        Document obj = cursor.next();
-        fillMap(resultMap, obj);
-
-        result.add(resultMap);
-      }
-
-      return Status.OK;
-    } catch (Exception e) {
-      System.err.println(e.toString());
-      return Status.ERROR;
-    } finally {
-      if (cursor != null) {
-        cursor.close();
-      }
+      result.add(map);
     }
-  }
-
-  protected void fillMap(Map<String, ByteIterator> resultMap, Document obj) {
-    for (Map.Entry<String, Object> entry : obj.entrySet()) {
-      if (entry.getValue() instanceof Binary) {
-        resultMap.put(entry.getKey(),
-                new ByteArrayByteIterator(((Binary) entry.getValue()).getData()));
-      }
-    }
+    return Status.OK;
   }
 
   @Override
-  public Status update(String table, String key, Map<String, ByteIterator> values) {
-    try {
-      MongoCollection<Document> collection = testDB.getCollection(table);
-
-      Document query = new Document(PK, key);
-      Document fieldsToSet = new Document();
-      for (Map.Entry<String, ByteIterator> entry : values.entrySet()) {
-        fieldsToSet.put(entry.getKey(), entry.getValue().toArray());
-      }
-      Document update = new Document("$set", fieldsToSet);
-
-      UpdateResult result = collection.updateOne(query, update);
-      if (result.wasAcknowledged() && result.getMatchedCount() == 0) {
-        System.err.println("Nothing updated for key " + key);
-        return Status.NOT_FOUND;
-      }
-      return Status.OK;
-    } catch (Exception e) {
-      System.err.println(e.toString());
-      return Status.ERROR;
-    }
+  public Status update(String table, String key,
+                       Map<String, ByteIterator> values) {
+    insert(table, key, values);
+    return Status.OK;
   }
 
   @Override
-  public Status insert(String table, String key, Map<String, ByteIterator> values) {
-    MongoCollection<Document> numbersCollection = testDB.getCollection(table);
-    Document doc = new Document(PK, key);
-    for (Map.Entry<String, ByteIterator> value : values.entrySet()) {
-      doc.append(value.getKey(), value.getValue().toArray());
+  public Status insert(String table, String key,
+                       Map<String, ByteIterator> values) {
+    // Add a vertex.
+    // Note that a Gremlin terminal step, e.g. next(),
+    // is required to make a request to the remote server.
+    // The full list of Gremlin terminal steps is at
+    // https://tinkerpop.apache.org/docs/current/reference/#terminal-steps
+    // g.addV("Person").property("Name", "Justin").next();
+    GraphTraversal<Vertex, Vertex> curr = g.addV(key);
+    for (Map.Entry<String, ByteIterator> entry : values.entrySet()) {
+      curr.property(entry.getKey(), entry.getValue().toString());
     }
-    numbersCollection.insertOne(doc);
+    curr.next();
     return Status.OK;
   }
 
   @Override
   public Status delete(String table, String key) {
-    try {
-      MongoCollection<Document> collection = testDB.getCollection(table);
-
-      Document query = new Document(PK, key);
-      DeleteResult result = collection.deleteOne(query);
-      if (result.wasAcknowledged() && result.getDeletedCount() == 0) {
-        System.err.println("Nothing deleted for key " + key);
-        return Status.NOT_FOUND;
-      }
-      return Status.OK;
-    } catch (Exception e) {
-      System.err.println(e.toString());
-      return Status.ERROR;
-    }
+    g.V(key).drop();
+    return Status.OK;
   }
 
 }
